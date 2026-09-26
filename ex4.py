bb49c35f732234bcc8eb41836bc3c7c6a9a09f63
Túlio Gomes de Abreu <art0072428@aluno.firjansenaisesi.com.br>
2026-09-25 21:20:54 -0300

    codico01

--- ex4.py
+++ ex4.py
@@ -7,4 +7,7 @@ for i in range(0,4):
     for aluno_nota in lista:
         if aluno[1] >= 6:
             print(aluno[0])
-            
+
+        # for i in rang(0, len(lista)): versão 2
+        # if lista[i][1] >=6:
+        # print(lista[i][0])    
